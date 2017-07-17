70f8fa92c83b5fb906481c43aed6b554b9e75448
kshah3318 <kshah3318@gmail.com>
2017-07-17 15:44:38 +0530

    Completing authentication using firebase.Authentication of signin and singup completed perfectly with firebase...

--- app/src/main/java/com/karan/firebasesignin/MainActivity.java
+++ app/src/main/java/com/karan/firebasesignin/MainActivity.java
@@ -1,16 +1,22 @@
 package com.karan.firebasesignin;
-
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 public class MainActivity extends AppCompatActivity {
-      private FirebaseAuth firebaseAuth;
+    private FirebaseAuth firebaseAuth;
     private EditText etUserName,etEmail,etPassword;
     private Button sign_in,sign_up;
     @Override
@@ -27,17 +33,16 @@ public class MainActivity extends AppCompatActivity {
         sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+               CallSignIn(etEmail.getText().toString(),etPassword.getText().toString());
             }
         });
         sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                CallSignUp(etEmail.getText().toString(),etPassword.getText().toString());
             }
         });
     }
-
     private  void CheckedLoggedIn()
     {
         if(firebaseAuth.getCurrentUser()!=null)
@@ -46,5 +51,57 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(getApplicationContext(),SignIn.class));
         }
     }
+    private void CallSignUp(String email,String password)
+    {
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+               if(!task.isSuccessful())
+               {
+                   Toast.makeText(MainActivity.this,"Signup Failed",Toast.LENGTH_SHORT).show();
+               }
+               else
+               {
+                   userProfile();
+                   Toast.makeText(MainActivity.this,"Signup Successful",Toast.LENGTH_SHORT).show();
+               }
 
+            }
+        });
+
+    }
+    private  void CallSignIn(String email,String password)
+    {
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+               if(!task.isSuccessful())
+               {
+                   Toast.makeText(MainActivity.this,"FAILED", Toast.LENGTH_SHORT).show();
+               }
+               else
+               {
+                   Intent i=new Intent(MainActivity.this,SignIn.class);
+                   finish();
+                   startActivity(i);
+               }
+            }
+        });
+    }
+    private void userProfile() {
+        FirebaseUser user=firebaseAuth.getCurrentUser();
+        if(user!=null)
+        {
+            UserProfileChangeRequest profileUpdates=new UserProfileChangeRequest.Builder().setDisplayName(etUserName.getText().toString().trim()).build();
+            user.updateProfile(profileUpdates).addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful())
+                    {
+                        Log.d("TESTING","User Profile Updated");
+                    }
+                }
+            });
+        }
+    }
 }
--- app/src/main/java/com/karan/firebasesignin/SignIn.java
+++ app/src/main/java/com/karan/firebasesignin/SignIn.java
@@ -1,13 +1,45 @@
 package com.karan.firebasesignin;
-
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
 public class SignIn extends AppCompatActivity {
-
+    private FirebaseAuth firebaseAuth;
+    Button signout;
+    TextView username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        firebaseAuth=FirebaseAuth.getInstance();
+        signout=(Button)findViewById(R.id.btn_signout);
+        username=(TextView)findViewById(R.id.disp_username);
+        //Again Check if the user is already logged in or not
+        if(firebaseAuth.getCurrentUser()==null)
+        {
+            finish();
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        }
+        //Fetch the display name of the current user
+        FirebaseUser user=firebaseAuth.getCurrentUser();
+        if(user!=null)
+        {
+            username.setText("Welcome"+""+user.getDisplayName());
+        }
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                finish();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
     }
 }
